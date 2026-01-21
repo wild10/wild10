cf0699b02a6ee5112470eac7cab7828c202d7f69
wild10 <wildr.10@gmail.com>
2026-01-20 21:43:39 -0500

    sum of 2 numbers

---
+++ python/leetcode/book.py
@@ -0,0 +1,12 @@
+## ///////////////////////////////////////////
+## this is a book 
+## ///////////////////////////////////////////
+
+def sum(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    A = int(input())
+    B = int(input()) 
+    print(f"suma is : {sum(A, B)}") 
+    print("hello world")

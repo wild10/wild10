b81134b0ad8764eb6149d8b1231a094b8097439e
wild10 <wildr.10@gmail.com>
2026-01-30 16:37:53 -0500

    update tribonacci dp using memo

--- python/leetcode/1137-Ntribonacci.py
+++ python/leetcode/1137-Ntribonacci.py
@@ -3,6 +3,20 @@
 #   Author: wild10
 #///////////////////////////////////////
 
+memo = {}
+# usind memo DP approach  : O(n)
+def dp_tribonacci(n):
+    if n == 0:
+        return 0:
+    if n == 1 or n == 2:
+        return 1
+    # if n already i memo -> return value
+    if n in memo:
+        return memo[n]
+    #else calculate new value and return 
+    memo[n] = dp_tribonacci(n-1) + dp_tribonacci(n-2) + dp_tribonacci(n-3)
+    return memo[n]
+    
 # Recursive function: O(n^2)
 def n_tribonacci(n):
     if n == 0:
